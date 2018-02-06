616dba6d037f61d6294c7ea2280ede0024c6c0ca
Surender Neelakantan <surender.neelakantan@gmail.com>
2018-02-06 13:21:39 +0530

    ps

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'app/product.service';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-description',
@@ -8,9 +8,9 @@ import { ProductService } from 'app/product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  constructor(private _productService: ProductService) { }
-
   albumInfo;
+  
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
