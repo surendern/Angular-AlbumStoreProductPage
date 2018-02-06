5d4ad8367886a93a0816fd204de41918eecd9d7d
Surender Neelakantan <surender.neelakantan@gmail.com>
2018-02-06 14:32:40 +0530

    Pluralsight Part 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
+import { Product } from './product';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
@@ -10,9 +11,14 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   private _albumUrl: string = '../assets/album.json';
+  private _productsUrl: string = '../assets/products.json';
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map(response => <Album> response.json());
+  }
+
+  getProducts(id: number): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map(response => <Product[]> response.json());
   }
 
 }
